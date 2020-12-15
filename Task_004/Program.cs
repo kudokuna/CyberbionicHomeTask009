e8a70c2ad3a8d29a0f42d49b61416914e0390e14
htcak <htcakuma@gmail.com>
2020-12-16 00:56:06 +0200

    Add Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_004
+{
+    class Program
+    {
+        static void ShowCreatedArray(int[] arr)
+        {
+            foreach (var value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.Write("\n");
+        }
+        static int[] CreateAnotherArray(int[] arr, int value) 
+        {
+            int[] resultArray = new int[arr.Length + 1];
+            resultArray[0] = value; 
+            for (int i = 1; i < resultArray.Length; i++)
+            {
+                resultArray[i] = arr[i - 1];
+            }
+
+            return resultArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 1, 3, 4, 6, 7, 8, 6, 5, 8, 12};
+
+            ShowCreatedArray(arr);
+            int[] newArr = CreateAnotherArray(arr, 99);
+            ShowCreatedArray(newArr);
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
